930d76246b94132ff5ec0a292b68ee5b79d5c1e9
Matthew Adamson <matthewladamson@gmail.com>
2020-11-12 18:43:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,31 @@
+// Dependencies
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 80;
-
+// Setting up Express App
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(morgan("dev"));
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static("public"));
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WorkoutTracker";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
 
+// Creating Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WorkoutTracker";mongoose.connect(MONGODB_URI, {    useNewUrlParser: true,    useFindAndModify: false})
-
-// mongoose.connect(
-//  process.env.MONGODB_URI || 'mongodb://localhost/WorkoutTracker',
- // {
- //   useNewUrlParser: true,
- //   useUnifiedTopology: true,
- //   useCreateIndex: true,
- //   useFindAndModify: false
-// }
-// );
-const db = mongoose.connection; //create db variable
-db.on("error", console.error.bind(console, "Connection error:")); //if any error occurs
-
-
-// routes
-
-app.use(require("./routes/htmlRoutes.js"));
-app.use(require("./routes/apiRoutes.js"));
-
-// db.once("open", function () {
- // console.log("Connected to MongoDB");
- app.listen(PORT, function () {
-   console.log(`App running on port http://localhost:${PORT}`);
- });
-// })
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
+});
