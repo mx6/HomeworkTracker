632b8d439b71762c7c5fea0e543cb6a76e6987f8
Matthew Adamson <matthewladamson@gmail.com>
2020-11-13 11:24:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WorkoutTracker";mongoose.connect(MONGODB_URI, {    useNewUrlParser: true,    useFindAndModify: false})
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/WorkoutTracker";mongoose.connect(MONGODB_URI, {    useNewUrlParser: true,    useFindAndModify: false})
 
-// mongoose.connect(
-//  process.env.MONGODB_URI || 'mongodb://localhost/WorkoutTracker',
- // {
- //   useNewUrlParser: true,
- //   useUnifiedTopology: true,
- //   useCreateIndex: true,
- //   useFindAndModify: false
-// }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/WorkoutTracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+ }
+ );
 const db = mongoose.connection; //create db variable
 db.on("error", console.error.bind(console, "Connection error:")); //if any error occurs
 
