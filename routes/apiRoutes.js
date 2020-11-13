4e75ed1446a0b5022d6db83e8896ad83647b4c77
Matthew Adamson <matthewladamson@gmail.com>
2020-11-13 12:38:09 -0800

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,7 +38,7 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  Workout.find().limit(7)
+  Workout.find()
     .sort({ natural: 1 })
     .then(dbWorkout => {
       res.json(dbWorkout);
