04f4bbaf5193174531b03b97c425bcf33ac567de
Matthew Adamson <matthewladamson@gmail.com>
2020-11-12 18:38:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";mongoose.connect(MONGODB_URI, {    useNewUrlParser: true,    useFindAndModify: false})
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/WorkoutTracker',
   {
